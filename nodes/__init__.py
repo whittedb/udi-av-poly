f34cdb9caebf2afbdb486cf12ca901cef0c16089
Brad Whitted <whittedb@the-zoo.net>
2020-10-16 03:09:59 -0500

    - Fix missed refactor of bravia file and class name

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -6,5 +6,5 @@ import polyinterface
 from .av_controller import AVController
 from .av_node import AVNode
 from .pioneer_vsx1021_node import PioneerVSX1021Node
-from .sony_bravia_xbr_65x810c_node import SonyBraviaXBR65X810CNode
+from .sony_bravia_xbr_node import SonyBraviaXBRNode
 from .node_factory import NodeFactory
